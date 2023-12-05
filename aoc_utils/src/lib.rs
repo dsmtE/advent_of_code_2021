ba0a6d40e94d35a7d5ff2fb4fc2c792a71952698
dsmtE <desmet.enguerrand@gmail.com>
2023-12-05 02:15:19 +0100

    ✨ Proc_macro support using sub crate in aoc_utils

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -3,6 +3,8 @@ mod regex;
 
 mod iterator_helpers;
 
+pub use aoc_utils_proc_macro::*;
+
 #[macro_export]
 macro_rules! get_input { () => (
     include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/", env!("CARGO_BIN_NAME"), ".txt"))
