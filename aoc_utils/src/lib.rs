bf67ceb49f4f86d9994b3791e1f471eea6ce57a1
Enguerrand De Smet <enguerrand.de-smet@ubisoft.com>
2023-12-13 16:31:25 +0100

    ✨ add grid utils in aoc_utils

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -6,6 +6,9 @@ pub mod nom_helpers;
 
 pub use aoc_utils_proc_macro::*;
 
+mod grid;
+pub use grid::*;
+
 #[macro_export]
 macro_rules! get_input { () => (
     include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/", env!("CARGO_BIN_NAME"), ".txt"))
