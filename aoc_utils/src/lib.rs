d64bc542d6d1351ec9fef70d1da9882bd926048f
dsmtE <desmet.enguerrand@gmail.com>
2023-12-23 00:22:05 +0100

    ✨ [aoc_utils] add to_decimal parsing from char

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -11,6 +11,11 @@ pub use aoc_utils_proc_macro::*;
 mod grid;
 pub use grid::*;
 
+pub fn to_decimal(c: char) -> Option<u32> {
+    let as_number = (c as u32).wrapping_sub('0' as u32);
+    if as_number < 10 { Some(as_number) } else { None }
+}
+
 #[macro_export]
 macro_rules! get_input { () => (
     include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/", env!("CARGO_BIN_NAME"), ".txt"))
