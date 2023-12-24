f2528237b77f7f03d0d510b35487f82f5a9f3b76
dsmtE <desmet.enguerrand@gmail.com>
2023-12-24 02:58:54 +0100

    🚧 [21] not working naive second star

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,6 +1,7 @@
 const INPUT: &str = aoc_utils::get_input!();
 use std::collections::BTreeSet;
 
+use aoc_utils::cartesian::{p2, Point2, Direction, Cartesian2};
 use aoc_utils::{parse_char_grid, iterator::IterUtils, Grid, in_bound_cardinal_neighbors_index};
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
@@ -24,6 +25,35 @@ fn reachable_neighbors_from_pos(starting_pos_index: usize, map: &Grid<char>, ste
             .collect_vec();
     }
     positions_index    
+}
+
+fn reachable_neighbors_infinity(position: Point2, map: &Grid<char>) -> Vec<Point2> {
+    let (width, height) = (map.width as i64, map.height as i64);
+    vec![
+        position + Direction::Up.vector(),
+        position + Direction::Down.vector(),
+        position + Direction::Left.vector(),
+        position + Direction::Right.vector(),
+    ]
+    .into_iter()
+    .filter(|&p| {
+        map[(p.x().rem_euclid(width) as usize, p.y().rem_euclid(height) as usize)] != '#'
+    })
+    .collect_vec()
+}
+
+fn reachable_neighbors_from_pos_infinity(starting_pos: Point2, map: &Grid<char>, step_count: usize) -> Vec<Point2> {
+    let mut positions = vec![starting_pos];
+    for _ in 0..step_count {
+        positions = 
+        positions
+            .par_iter()
+            .flat_map(|&pos| reachable_neighbors_infinity(pos, &map))
+            .collect::<BTreeSet<_>>()
+            .into_iter()
+            .collect_vec();
+    }
+    positions    
 }
 
 fn main() {
@@ -55,5 +85,19 @@ mod tests {
         let starting_pos_index = map.data.iter().enumerate().find(|(_, &c)| c == 'S').unwrap().0;
 
         assert_eq!(reachable_neighbors_from_pos(starting_pos_index, &map, 6).len(), 16);
+    }
+
+    #[test]
+    fn second_start() {
+        let map = parse_char_grid(TEST_INPUT);
+        let starting_pos_index = map.data.iter().enumerate().find(|(_, &c)| c == 'S').unwrap().0;
+
+        let starting_pos = p2((starting_pos_index % map.width) as i64, (starting_pos_index / map.width) as i64);
+
+        for (step, expected) in vec![(6, 16), (50, 1594), (100, 6536), (500, 167004), (1000, 668697)] {
+            println!("step: {}", step);
+            assert_eq!(reachable_neighbors_from_pos_infinity(starting_pos, &map, step).len(), expected);
+        }
+        
     }
 }
